8de268cd3989d3021fb81574e343b2c0a5e7204d
Radhika <radhika.shankar1220@gmail.com>
2021-07-08 19:08:48 +0530

    [Radhika] insertion operation

---
+++ LinkedListUsingGenerics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListUsingGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********1.Inserted into list(From Last)************");
+            int num = Convert.ToInt32(Console.ReadLine());
+            //Creating object for Custom Linked list class
+            CustomLinkedList custom = new CustomLinkedList();
+            switch(num)
+            {
+                case 1:
+                    custom.InsertLast(56);
+                    custom.InsertLast(30);
+                    custom.InsertLast(70);
+                    custom.Display();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
